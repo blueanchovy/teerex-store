ac70f49b55ae6eaf122eba056e667a143572d822
Manish Jha <manishjhazyx@gmail.com>
2023-05-06 04:50:03 +0530

    added data fetch from api

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "styles/pages/Home.module.scss";
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
   return (
     <div>
       <Head>
@@ -13,7 +14,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Content />
+      <Content cardsData={data} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const response = await fetch(
+    "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json"
+  );
+  const data = await response.json();
+  return {
+    props: { data },
+  };
 }
