6269db744cf56b0a7ebbf37b438075c8657d7c89
Manish Jha <manishjhazyx@gmail.com>
2023-05-29 15:19:57 +0530

    fix: productcard details spacing

--- components/Products/ProductCard.js
+++ components/Products/ProductCard.js
@@ -27,58 +27,64 @@ function ProductCard(props) {
           }}
           className={ProductCardClasses["productCard__img"]}
         />
-        <div className={ProductCardClasses["productCard__name"]}>
-          {cardData.name}
-        </div>
-        <div className={ProductCardClasses["productCard__summary"]}>
-          <div className={ProductCardClasses["productCard__price"]}>
-            {cardData.currency + " " + cardData.price}
+        <div className={ProductCardClasses["productCard__details"]}>
+          <div className={ProductCardClasses["productCard__name"]}>
+            {cardData.name}
           </div>
-          {!itemCountInCart ? (
-            <div
-              className={ProductCardClasses["productCard__addToCartButton"]}
-              onClick={handleAddToCart}
-            >
-              <div className={ProductCardClasses["productCard__addToCartText"]}>
-                Add to Cart
-              </div>
+          <div className={ProductCardClasses["productCard__summary"]}>
+            <div className={ProductCardClasses["productCard__price"]}>
+              {cardData.currency + " " + cardData.price}
+            </div>
+            {!itemCountInCart ? (
+              <div
+                className={ProductCardClasses["productCard__addToCartButton"]}
+                onClick={handleAddToCart}
+              >
+                <div
+                  className={ProductCardClasses["productCard__addToCartText"]}
+                >
+                  Add to Cart
+                </div>
 
-              <Image
-                src="/cart-shopping-solid.svg"
-                width={22}
-                height={22}
-                alt="cart"
-              />
-            </div>
-          ) : (
-            <div className={ProductCardClasses["productCard__itemStepper"]}>
-              <div
-                className={ProductCardClasses["productCard__itemStepperLess"]}
-                onClick={() => {
-                  if (itemCountInCart > 0) {
-                    setItemCountInCart(itemCountInCart - 1);
+                <Image
+                  src="/cart-shopping-solid.svg"
+                  width={22}
+                  height={22}
+                  alt="cart"
+                />
+              </div>
+            ) : (
+              <div className={ProductCardClasses["productCard__itemStepper"]}>
+                <div
+                  className={ProductCardClasses["productCard__itemStepperLess"]}
+                  onClick={() => {
+                    if (itemCountInCart > 0) {
+                      setItemCountInCart(itemCountInCart - 1);
+                    }
+                  }}
+                >
+                  &minus;
+                </div>
+                <div
+                  className={
+                    ProductCardClasses["productCard__itemStepperCount"]
                   }
-                }}
-              >
-                &minus;
+                >
+                  {itemCountInCart}
+                </div>
+                <div
+                  className={ProductCardClasses["productCard__itemStepperMore"]}
+                  onClick={() => {
+                    if (itemCountInCart < cardData.quantity) {
+                      setItemCountInCart(itemCountInCart + 1);
+                    }
+                  }}
+                >
+                  &#43;
+                </div>
               </div>
-              <div
-                className={ProductCardClasses["productCard__itemStepperCount"]}
-              >
-                {itemCountInCart}
-              </div>
-              <div
-                className={ProductCardClasses["productCard__itemStepperMore"]}
-                onClick={() => {
-                  if (itemCountInCart < cardData.quantity) {
-                    setItemCountInCart(itemCountInCart + 1);
-                  }
-                }}
-              >
-                &#43;
-              </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </>
