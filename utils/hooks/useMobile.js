79299d4777abc41c0fd1eb846fdf082d4dbb5731
Manish Jha <manishjhazyx@gmail.com>
2023-05-31 20:18:19 +0530

    fix: error handling in useMobile hook

--- utils/hooks/useMobile.js
+++ utils/hooks/useMobile.js
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
 
 export default function useMobile() {
-  const [isTabletOrSmaller, setIsTabletOrSmaller] = useState(
-    window.matchMedia("(max-width: 1023px)").matches
-  );
-  const [isMobileOrSmaller, setIsMobileOrSmaller] = useState(
-    window.matchMedia("(max-width: 767px)").matches
-  );
+  const [isTabletOrSmaller, setIsTabletOrSmaller] = useState(false);
+  const [isMobileOrSmaller, setIsMobileOrSmaller] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsTabletOrSmaller(window.matchMedia("(max-width: 1023px)").matches);
-      setIsMobileOrSmaller(window.matchMedia("(max-width: 767px)").matches);
+      setIsTabletOrSmaller(
+        window?.matchMedia("(max-width: 1023px)")?.matches || false
+      );
+      setIsMobileOrSmaller(
+        window?.matchMedia("(max-width: 767px)")?.matches || false
+      );
     };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    if (typeof window !== "undefined") {
+      setIsTabletOrSmaller(
+        window?.matchMedia("(max-width: 1023px)")?.matches || false
+      );
+      setIsMobileOrSmaller(
+        window?.matchMedia("(max-width: 767px)")?.matches || false
+      );
+
+      window?.addEventListener("resize", handleResize);
+
+      return () => {
+        window?.removeEventListener("resize", handleResize);
+      };
+    }
   }, []);
 
   return {
